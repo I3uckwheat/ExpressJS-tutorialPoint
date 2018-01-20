b2a18ef52601d4849bbc028491bb55e64322ec06
Briggs Elsperger <i3uckwheat@gmail.com>
2018-01-19 19:11:46 -0600

    dynamic routes

--- index.js
+++ index.js
@@ -5,4 +5,8 @@ const things = require("./things.js")
 
 app.use("/things", things)
 
+app.get(':id', function(req, res) {
+  res.send("Thie id you specified is " + req.params.id)
+})
+
 app.listen(3000);
