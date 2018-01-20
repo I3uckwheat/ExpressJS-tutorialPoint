1aee10112b9bd8b78b4e7372f080f77eab8a014f
Briggs Elsperger <i3uckwheat@gmail.com>
2018-01-19 19:37:16 -0600

    adds view example

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+app.set("view engine", "pug");
+app.set("views", "./views");
 
 const things = require("./things.js")
 
@@ -7,6 +9,10 @@ app.use("/things", things)
 
 app.get(':id', function(req, res) {
   res.send("Thie id you specified is " + req.params.id)
+})
+
+app.get("/first_template", (req, res) => {
+  res.render("first_view")
 })
 
 app.listen(3000);
